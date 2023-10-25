8434e04d5dec69672dd1021f109ba9b4a5ddbf9a
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-10-25 10:07:55 -0600

    Renamed MainForm

--- EngineeringManagement.UI/MainForm.cs
+++ EngineeringManagement.UI/MainForm.cs
@@ -2,9 +2,9 @@ using System.ComponentModel;
 
 namespace EngineeringManagement.UI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
