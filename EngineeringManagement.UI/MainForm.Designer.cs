18b4976a009bf72738f0d82daef2ba5e18681471
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-10-27 16:54:08 -0600

    Added logic to load data in main grid

--- EngineeringManagement.UI/MainForm.Designer.cs
+++ EngineeringManagement.UI/MainForm.Designer.cs
@@ -34,24 +34,31 @@
             agregarEmpleadoToolStripMenuItem = new ToolStripMenuItem();
             editarEmpleadoToolStripMenuItem = new ToolStripMenuItem();
             plantasToolStripMenuItem = new ToolStripMenuItem();
+            dC3ToolStripMenuItem = new ToolStripMenuItem();
+            agregarDC3ToolStripMenuItem = new ToolStripMenuItem();
+            editarDC3ToolStripMenuItem = new ToolStripMenuItem();
+            agregarDC3AEmpleadoToolStripMenuItem = new ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)dgvEmployees).BeginInit();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // dgvEmployees
             // 
+            dgvEmployees.AllowUserToAddRows = false;
+            dgvEmployees.AllowUserToDeleteRows = false;
             dgvEmployees.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvEmployees.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvEmployees.Dock = DockStyle.Bottom;
-            dgvEmployees.Location = new Point(0, 157);
+            dgvEmployees.Dock = DockStyle.Fill;
+            dgvEmployees.Location = new Point(0, 24);
             dgvEmployees.Name = "dgvEmployees";
+            dgvEmployees.ReadOnly = true;
             dgvEmployees.RowTemplate.Height = 25;
-            dgvEmployees.Size = new Size(956, 331);
+            dgvEmployees.Size = new Size(956, 464);
             dgvEmployees.TabIndex = 0;
             // 
             // menuStrip1
             // 
-            menuStrip1.Items.AddRange(new ToolStripItem[] { empleadosToolStripMenuItem, plantasToolStripMenuItem });
+            menuStrip1.Items.AddRange(new ToolStripItem[] { empleadosToolStripMenuItem, plantasToolStripMenuItem, dC3ToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
             menuStrip1.Size = new Size(956, 24);
@@ -83,6 +90,31 @@
             plantasToolStripMenuItem.Size = new Size(57, 20);
             plantasToolStripMenuItem.Text = "Plantas";
             // 
+            // dC3ToolStripMenuItem
+            // 
+            dC3ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { agregarDC3ToolStripMenuItem, editarDC3ToolStripMenuItem, agregarDC3AEmpleadoToolStripMenuItem });
+            dC3ToolStripMenuItem.Name = "dC3ToolStripMenuItem";
+            dC3ToolStripMenuItem.Size = new Size(46, 20);
+            dC3ToolStripMenuItem.Text = "DC-3";
+            // 
+            // agregarDC3ToolStripMenuItem
+            // 
+            agregarDC3ToolStripMenuItem.Name = "agregarDC3ToolStripMenuItem";
+            agregarDC3ToolStripMenuItem.Size = new Size(211, 22);
+            agregarDC3ToolStripMenuItem.Text = "Agregar DC-3";
+            // 
+            // editarDC3ToolStripMenuItem
+            // 
+            editarDC3ToolStripMenuItem.Name = "editarDC3ToolStripMenuItem";
+            editarDC3ToolStripMenuItem.Size = new Size(211, 22);
+            editarDC3ToolStripMenuItem.Text = "Editar DC-3";
+            // 
+            // agregarDC3AEmpleadoToolStripMenuItem
+            // 
+            agregarDC3AEmpleadoToolStripMenuItem.Name = "agregarDC3AEmpleadoToolStripMenuItem";
+            agregarDC3AEmpleadoToolStripMenuItem.Size = new Size(211, 22);
+            agregarDC3AEmpleadoToolStripMenuItem.Text = "Agregar DC-3 a Empleado";
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -110,5 +142,9 @@
         private ToolStripMenuItem agregarEmpleadoToolStripMenuItem;
         private ToolStripMenuItem editarEmpleadoToolStripMenuItem;
         private ToolStripMenuItem plantasToolStripMenuItem;
+        private ToolStripMenuItem dC3ToolStripMenuItem;
+        private ToolStripMenuItem agregarDC3ToolStripMenuItem;
+        private ToolStripMenuItem editarDC3ToolStripMenuItem;
+        private ToolStripMenuItem agregarDC3AEmpleadoToolStripMenuItem;
     }
 }
--- EngineeringManagement.UI/MainForm.cs
+++ EngineeringManagement.UI/MainForm.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
 
 namespace EngineeringManagement.UI
@@ -15,9 +16,27 @@ namespace EngineeringManagement.UI
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-            dgvEmployees.DataSource = context.Employees
-                .Select(r => new { r.Position, r.EmployeeName })
+            dgvEmployees.DataSource = context.EmployeeCertifications
+                .Include(r => r.Employee)
+                .Include(r => r.Certification)
+                .Where(r => r.StartDate.HasValue && DateTime.Now.AddDays(-10) > r.EndDate)
+                .Select(r => new
+                {
+                    r.Employee.EmployeeName,
+                    r.Certification.CertificationName,
+                    r.StartDate,
+                    r.EndDate
+                })
                 .ToList();
+            FormatColumnHeaders();
+        }
+
+        private void FormatColumnHeaders()
+        {
+            dgvEmployees.Columns[0].HeaderText = "Colaborador";
+            dgvEmployees.Columns[1].HeaderText = "Puesto";
+            dgvEmployees.Columns[2].HeaderText = "Inicio";
+            dgvEmployees.Columns[3].HeaderText = "Vencimiento";
         }
 
         protected override void OnClosing(CancelEventArgs e)
