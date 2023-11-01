5727e162d59f68f2fb09bedfdd3ea09e786b5c9d
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-11-01 12:50:36 -0600

    Completed AddCertification form

--- EngineeringManagement.UI/Forms/Certifications/AddCertificaction.cs
+++ EngineeringManagement.UI/Forms/Certifications/AddCertificaction.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EngineeringManagement.Data.Models;
+using EngineeringManagement.UI.Extensions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,43 @@ namespace EngineeringManagement.UI.Forms
         public AddCertificaction()
         {
             InitializeComponent();
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            if (!TxtCertName.Text.HasValue())
+            {
+                MessageBox.Show("Nombre es un campo requerido", "Agregar DC-3", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+            var certification = new Certification
+            {
+                CertificationName = TxtCertName.Text,
+                Description = TxtCertDesc.Text,
+            };
+            using (var context = new Data.AppContext())
+            {
+                try
+                {
+                    context.Certifications.Add(certification);
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    MessageBox.Show("Guardado con exito.", "Agregar DC-3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+            }
         }
     }
 }
