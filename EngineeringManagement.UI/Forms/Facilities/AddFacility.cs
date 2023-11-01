b90f59b027c160aa0b586dace21de5ee264162ce
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-10-31 18:01:01 -0600

    Added AddFacility form

---
+++ EngineeringManagement.UI/Forms/Facilities/AddFacility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EngineeringManagement.UI.Forms.Facilities
+{
+    public partial class AddFacility : Form
+    {
+        public AddFacility()
+        {
+            InitializeComponent();
+        }
+    }
+}
