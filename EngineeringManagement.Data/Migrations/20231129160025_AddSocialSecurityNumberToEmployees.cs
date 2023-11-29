22a09e662286cfd3978523106c87dffd41293131
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-11-29 10:00:59 -0600

    Added new field to Employee

---
+++ EngineeringManagement.Data/Migrations/20231129160025_AddSocialSecurityNumberToEmployees.cs
@@ -0,0 +1,309 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EngineeringManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSocialSecurityNumberToEmployees : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SocialSecurityNumber",
+                table: "Employees",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -40,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -39,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -38,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -37,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -36,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -35,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -34,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -33,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -32,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -31,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -30,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -29,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -28,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -27,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -26,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -25,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -24,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -23,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -22,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -21,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -20,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -19,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -18,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -17,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -16,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -15,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -14,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -13,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -12,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -11,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -10,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -9,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -8,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -7,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -6,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -5,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -4,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -3,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -2,
+                column: "SocialSecurityNumber",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "SocialSecurityNumber",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SocialSecurityNumber",
+                table: "Employees");
+        }
+    }
+}
