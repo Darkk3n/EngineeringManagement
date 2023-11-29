3cba8b742bcda128e35d9f05da5d3570e4c0fd56
Gerardo Aguilar <gerardo.aguilar@petco.com>
2023-11-29 11:56:41 -0600

    Added controls

--- EngineeringManagement.UI/Forms/Employees/EmployeeMedFile.Designer.cs
+++ EngineeringManagement.UI/Forms/Employees/EmployeeMedFile.Designer.cs
@@ -28,12 +28,296 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "EmployeeMedFile";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EmployeeMedFile));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            CmbEmployees = new ComboBox();
+            LblCurp = new Label();
+            LblNss = new Label();
+            CmbAble = new ComboBox();
+            BtnViewSisosit = new Button();
+            BtnViewLabs = new Button();
+            label11 = new Label();
+            textBox1 = new TextBox();
+            maskedTextBox1 = new MaskedTextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            BtnOk = new Button();
+            BtnCancel = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(85, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Empleado:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(108, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 1;
+            label2.Text = "CURP:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(117, 72);
+            label3.Name = "label3";
+            label3.Size = new Size(31, 15);
+            label3.TabIndex = 2;
+            label3.Text = "NSS:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(60, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(88, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Tipo de Sangre:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 129);
+            label5.Name = "label5";
+            label5.Size = new Size(136, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Telefono de Emergencia:";
+            // 
+            // label6
+            // 
+            label6.Location = new Point(23, 154);
+            label6.Name = "label6";
+            label6.Size = new Size(125, 39);
+            label6.TabIndex = 5;
+            label6.Text = "Nombre del Telefono de Emergencia:";
+            label6.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(80, 203);
+            label7.Name = "label7";
+            label7.Size = new Size(68, 15);
+            label7.TabIndex = 6;
+            label7.Text = "Parentesco:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(441, 8);
+            label8.Name = "label8";
+            label8.Size = new Size(36, 15);
+            label8.TabIndex = 7;
+            label8.Text = "Apto:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(409, 44);
+            label9.Name = "label9";
+            label9.Size = new Size(68, 15);
+            label9.TabIndex = 8;
+            label9.Text = "Ver SISOSIT:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(382, 76);
+            label10.Name = "label10";
+            label10.Size = new Size(95, 15);
+            label10.TabIndex = 9;
+            label10.Text = "Ver Laboratorios:";
+            // 
+            // CmbEmployees
+            // 
+            CmbEmployees.DropDownStyle = ComboBoxStyle.DropDownList;
+            CmbEmployees.FormattingEnabled = true;
+            CmbEmployees.Location = new Point(154, 5);
+            CmbEmployees.Name = "CmbEmployees";
+            CmbEmployees.Size = new Size(263, 23);
+            CmbEmployees.TabIndex = 10;
+            // 
+            // LblCurp
+            // 
+            LblCurp.AutoSize = true;
+            LblCurp.Location = new Point(154, 44);
+            LblCurp.Name = "LblCurp";
+            LblCurp.Size = new Size(12, 15);
+            LblCurp.TabIndex = 11;
+            LblCurp.Text = "_";
+            // 
+            // LblNss
+            // 
+            LblNss.AutoSize = true;
+            LblNss.Location = new Point(154, 72);
+            LblNss.Name = "LblNss";
+            LblNss.Size = new Size(12, 15);
+            LblNss.TabIndex = 12;
+            LblNss.Text = "_";
+            // 
+            // CmbAble
+            // 
+            CmbAble.DropDownStyle = ComboBoxStyle.DropDownList;
+            CmbAble.FormattingEnabled = true;
+            CmbAble.Location = new Point(483, 5);
+            CmbAble.Name = "CmbAble";
+            CmbAble.Size = new Size(121, 23);
+            CmbAble.TabIndex = 13;
+            // 
+            // BtnViewSisosit
+            // 
+            BtnViewSisosit.Image = Properties.Resources.Lupa_Icon__2_;
+            BtnViewSisosit.Location = new Point(483, 40);
+            BtnViewSisosit.Name = "BtnViewSisosit";
+            BtnViewSisosit.Size = new Size(42, 25);
+            BtnViewSisosit.TabIndex = 14;
+            BtnViewSisosit.UseVisualStyleBackColor = true;
+            // 
+            // BtnViewLabs
+            // 
+            BtnViewLabs.Image = Properties.Resources.Lupa_Icon__2_;
+            BtnViewLabs.Location = new Point(483, 72);
+            BtnViewLabs.Name = "BtnViewLabs";
+            BtnViewLabs.Size = new Size(42, 25);
+            BtnViewLabs.TabIndex = 15;
+            BtnViewLabs.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(394, 115);
+            label11.Name = "label11";
+            label11.Size = new Size(83, 15);
+            label11.TabIndex = 16;
+            label11.Text = "Recordatorios:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(154, 97);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(221, 23);
+            textBox1.TabIndex = 17;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(154, 126);
+            maskedTextBox1.Mask = "999-000-0000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(221, 23);
+            maskedTextBox1.TabIndex = 18;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(154, 163);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(221, 23);
+            textBox2.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(154, 200);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(221, 23);
+            textBox3.TabIndex = 20;
+            // 
+            // BtnOk
+            // 
+            BtnOk.Location = new Point(212, 255);
+            BtnOk.Name = "BtnOk";
+            BtnOk.Size = new Size(75, 23);
+            BtnOk.TabIndex = 21;
+            BtnOk.Text = "Guardar";
+            BtnOk.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            BtnCancel.Location = new Point(293, 255);
+            BtnCancel.Name = "BtnCancel";
+            BtnCancel.Size = new Size(75, 23);
+            BtnCancel.TabIndex = 22;
+            BtnCancel.Text = "Cancelar";
+            BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // EmployeeMedFile
+            // 
+            AcceptButton = BtnOk;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = BtnCancel;
+            ClientSize = new Size(616, 290);
+            Controls.Add(BtnCancel);
+            Controls.Add(BtnOk);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(label11);
+            Controls.Add(BtnViewLabs);
+            Controls.Add(BtnViewSisosit);
+            Controls.Add(CmbAble);
+            Controls.Add(LblNss);
+            Controls.Add(LblCurp);
+            Controls.Add(CmbEmployees);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "EmployeeMedFile";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Expedientes Medicos";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private ComboBox CmbEmployees;
+        private Label LblCurp;
+        private Label LblNss;
+        private ComboBox CmbAble;
+        private Button BtnViewSisosit;
+        private Button BtnViewLabs;
+        private Label label11;
+        private TextBox textBox1;
+        private MaskedTextBox maskedTextBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button BtnOk;
+        private Button BtnCancel;
     }
 }
