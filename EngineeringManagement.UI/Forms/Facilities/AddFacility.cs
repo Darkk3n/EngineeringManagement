55682a633fad8e4dbbff23ecfe61e347a71357d2
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2024-07-05 10:24:17 -0600

    Added validation and message

--- EngineeringManagement.UI/Forms/Facilities/AddFacility.cs
+++ EngineeringManagement.UI/Forms/Facilities/AddFacility.cs
@@ -1,55 +1,58 @@
 ﻿using EngineeringManagement.Data.Models;
 using EngineeringManagement.UI.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace EngineeringManagement.UI.Forms
 {
-    public partial class AddFacility : Form
-    {
-        public AddFacility()
-        {
-            InitializeComponent();
-        }
+   public partial class AddFacility : Form
+   {
+      public AddFacility()
+      {
+         InitializeComponent();
+      }
 
-        private void BtnCancelar_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+      private void BtnCancelar_Click(object sender, EventArgs e)
+      {
+         this.Close();
+      }
 
-        private void BtnOk_Click(object sender, EventArgs e)
-        {
-            var newFacility = new Facility
+      private void BtnOk_Click(object sender, EventArgs e)
+      {
+         var newFacility = new Facility
+         {
+            FacilityName = TxtFacilityName.Text
+         };
+         using (var context = new Data.AppContext())
+         {
+            if (context.Facilities.Any(r => r.FacilityName.ToLower() == TxtFacilityName.Text.ToLower()))
             {
-                FacilityName = TxtFacilityName.Text
-            };
-            using (var context = new Data.AppContext())
-            {
-                try
-                {
-                    context.Facilities.Add(newFacility);
-                    context.SaveChanges();
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-                finally
-                {
-                    MessageBox.Show("Guardado con exito.", "Agregar Planta", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    this.Close();
-                }
+               MessageBox.Show($"La planta {TxtFacilityName.Text} ya existe.", "Agregar Planta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+               return;
             }
-        }
+            try
+            {
+               context.Facilities.Add(newFacility);
+               context.SaveChanges();
+               MessageBox.Show("Guardado con exito.", "Agregar Planta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+               this.Close();
+            }
+            catch (Exception)
+            {
+               throw;
+            }
+         }
+      }
 
-        private void TxtFacilityName_TextChanged(object sender, EventArgs e)
-        {
-            if (((TextBox)sender).Text.HasValue())
-            {
-                BtnOk.Enabled = true;
-            }
-            else
-            {
-                BtnOk.Enabled = false;
-            }
-        }
-    }
+      private void TxtFacilityName_TextChanged(object sender, EventArgs e)
+      {
+         if (((TextBox)sender).Text.HasValue())
+         {
+            BtnOk.Enabled = true;
+         }
+         else
+         {
+            BtnOk.Enabled = false;
+         }
+      }
+   }
 }
