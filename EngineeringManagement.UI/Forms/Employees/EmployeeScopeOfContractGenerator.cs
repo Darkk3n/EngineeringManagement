900f895d9bc95e8f09fdd6a3080d1f53de866053
Gerardo Aguilar <gerardo.aguilar@petco.com>
2025-08-06 16:27:50 -0600

    Initial setup

--- EngineeringManagement.UI/Forms/Employees/EmployeeScopeOfContractGenerator.cs
+++ EngineeringManagement.UI/Forms/Employees/EmployeeScopeOfContractGenerator.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EngineeringManagement.Data;
+using EngineeringManagement.Data.Models;
 
 namespace EngineeringManagement.UI.Forms.Employees
 {
     public partial class EmployeeScopeOfContractGenerator : Form
     {
-        public EmployeeScopeOfContractGenerator()
+        private readonly IServiceProvider serviceProvider;
+        private readonly HrDataContext context;
+        private List<GeneralEmployee> Employees { get; set; } = [];
+
+        public EmployeeScopeOfContractGenerator(IServiceProvider serviceProvider, HrDataContext context)
         {
             InitializeComponent();
-        }        
+            this.serviceProvider = serviceProvider;
+            this.context = context;
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            Employees.AddRange([.. context.GeneralEmployees]);
+            if (Employees.Count == 0)
+            {
+                cmbEmployees.Items.Add("-- NO SE ENCONTRARON EMPLEADOS --");
+                cmbEmployees.SelectedIndex = 0;
+            }
+            else
+            {
+                cmbEmployees.Items.Clear();
+                cmbEmployees.Items.AddRange([.. Employees.OrderBy(r => r.EmployeeName)]);
+                cmbEmployees.ValueMember = nameof(Employee.Id);
+                cmbEmployees.DisplayMember = nameof(Employee.EmployeeName);
+            }
+        }
     }
 }
