15c2f03b7f87839c996e3051893871b885511346
Gerardo Aguilar <gerardo.aguilar@petco.com>
2025-08-07 17:02:29 -0600

    WIP. Added global font, removed borders, set underline on values

--- EngineeringManagement.Core/Services/PdfGeneratorService.cs
+++ EngineeringManagement.Core/Services/PdfGeneratorService.cs
@@ -43,83 +43,85 @@ namespace EngineeringManagement.Core.Services
             var tableTitle = new Table(1).UseAllAvailableWidth();
             tableTitle.AddCell(new Cell()
                 .Add(new Paragraph("DATOS DE INGRESO DEL EMPLEADO (INFORMACIÓN LLENADA POR EL EMPLEADO)")
-                .SetFont(boldFont).SetFontSize(10))
+                .SetFont(boldFont)
+                .SetFontSize(10))
                 .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
                 .SetTextAlignment(TextAlignment.CENTER)
                 .SetPadding(5));
             document.Add(tableTitle);
 
             // Flexible info table layout (6 columns)
-            var table1 = new Table(6).UseAllAvailableWidth();
+            var table1 = new Table(6).UseAllAvailableWidth().SetFont(regularFont).SetFontSize(9);
 
-            table1.AddCell("Nombre:");
-            table1.AddCell("ERICK EUGENIO DEL VALLE BALDERAS");
-            table1.AddCell("Estado civil:");
-            table1.AddCell("SOLTERO");
-            table1.AddCell("Correo:");
-            table1.AddCell("");
+            table1.AddCell(NoBorderCell("Nombre:"));
+            table1.AddCell(NoBorderCell("ERICK EUGENIO DEL VALLE BALDERAS").SetUnderline());
+            table1.AddCell(NoBorderCell("Estado civil:"));
+            table1.AddCell(NoBorderCell("SOLTERO").SetUnderline());
+            table1.AddCell(NoBorderCell("Correo:"));
+            table1.AddCell(NoBorderCell("").SetUnderline());
 
-            table1.AddCell("Domicilio:");
-            table1.AddCell(new Cell(1, 5).Add(new Paragraph("AV LAS TORRES 2308 COL LAZARO CARDENAS CP. 89602 ALTAMIRA TAMPS.")));
+            table1.AddCell(NoBorderCell("Domicilio:"));
+            table1.AddCell(NoBorderCell("AV LAS TORRES 2308 COL LAZARO CARDENAS CP. 89602 ALTAMIRA TAMPS.", 5).SetUnderline());
 
-            table1.AddCell("NSS:");
-            table1.AddCell("38179900907");
-            table1.AddCell("No. Clínica IMSS:");
-            table1.AddCell("10");
-            table1.AddCell("Teléfono:");
-            table1.AddCell("833 323 5657");
+            table1.AddCell(NoBorderCell("NSS:"));
+            table1.AddCell(NoBorderCell("38179900907").SetUnderline());
+            table1.AddCell(NoBorderCell("No. Clínica IMSS:"));
+            table1.AddCell(NoBorderCell("10").SetUnderline());
+            table1.AddCell(NoBorderCell("Teléfono:"));
+            table1.AddCell(NoBorderCell("833 323 5657").SetUnderline());
 
-            table1.AddCell("No. Crédito Infonavit:");
-            table1.AddCell("NO APLICA");
-            table1.AddCell("% - VSM - Cuota:");
-            table1.AddCell("NO APLICA");
-            table1.AddCell("Número de cliente (Fonacot):");
-            table1.AddCell("NO APLICA");
+            table1.AddCell(NoBorderCell("No. Crédito Infonavit:"));
+            table1.AddCell(NoBorderCell("NO APLICA").SetUnderline());
+            table1.AddCell(NoBorderCell("% - VSM - Cuota:"));
+            table1.AddCell(NoBorderCell("NO APLICA").SetUnderline());
+            table1.AddCell(NoBorderCell("Número de cliente (Fonacot):"));
+            table1.AddCell(NoBorderCell("NO APLICA").SetUnderline());
 
-            table1.AddCell("CURP:");
-            table1.AddCell("VABE990508HTSLLR05");
-            table1.AddCell("RFC:");
-            table1.AddCell("VABE9905088J4");
-            table1.AddCell("Fecha de Nacimiento:");
-            table1.AddCell("08/05/1999");
+            table1.AddCell(NoBorderCell("CURP:"));
+            table1.AddCell(NoBorderCell("VABE990508HTSLLR05").SetUnderline());
+            table1.AddCell(NoBorderCell("RFC:"));
+            table1.AddCell(NoBorderCell("VABE9905088J4").SetUnderline());
+            table1.AddCell(NoBorderCell("Fecha de Nacimiento:"));
+            table1.AddCell(NoBorderCell("08/05/1999").SetUnderline());
 
-            table1.AddCell("Lugar de Nacimiento:");
-            table1.AddCell(new Cell(1, 5).Add(new Paragraph("CD MADERO TAMPS.")));
+            table1.AddCell(NoBorderCell("Lugar de Nacimiento:",2));
+            table1.AddCell(NoBorderCell("CD MADERO TAMPS.", 5).SetUnderline());
 
-            table1.AddCell("Grado acad.:");
-            table1.AddCell("");
-            table1.AddCell("Documento:");
-            table1.AddCell("");
-            table1.AddCell("Profesión:");
-            table1.AddCell("");
+            table1.AddCell(NoBorderCell("Grado acad."));
+            table1.AddCell(NoBorderCell("").SetUnderline());
+            table1.AddCell(NoBorderCell("Documento:"));
+            table1.AddCell(NoBorderCell("").SetUnderline());
+            table1.AddCell(NoBorderCell("Profesión:"));
+            table1.AddCell(NoBorderCell("").SetUnderline());
 
-            table1.AddCell("Nombre del padre:");
-            table1.AddCell(new Cell(1, 5).Add(new Paragraph("JAIME DEL VALLE OLMEDO")));
+            table1.AddCell(NoBorderCell("Nombre del padre:"));
+            table1.AddCell(NoBorderCell("JAIME DEL VALLE OLMEDO", 5).SetUnderline());
 
-            table1.AddCell("Nombre de la madre:");
-            table1.AddCell(new Cell(1, 5).Add(new Paragraph("ZOILA BALDERAS RAMIREZ")));
+            table1.AddCell(NoBorderCell("Nombre de la madre:"));
+            table1.AddCell(NoBorderCell("ZOILA BALDERAS RAMIREZ", 5).SetUnderline());
 
-            table1.AddCell("Beneficiario:");
-            table1.AddCell("ZOILA BALDERAS RAMIREZ");
-            table1.AddCell("%:");
-            table1.AddCell("100");
-            table1.AddCell("Teléfono beneficiario:");
-            table1.AddCell("833 104 2230");
+            table1.AddCell(NoBorderCell("Beneficiario:"));
+            table1.AddCell(NoBorderCell("ZOILA BALDERAS RAMIREZ").SetUnderline());
+            table1.AddCell(NoBorderCell("%:"));
+            table1.AddCell(NoBorderCell("100").SetTextAlignment(TextAlignment.LEFT).SetUnderline());
+            table1.AddCell(NoBorderCell("Teléfono beneficiario:"));
+            table1.AddCell(NoBorderCell("833 104 2230").SetUnderline());
 
-            table1.AddCell("Fecha de nacimiento:");
-            table1.AddCell(new Cell(1, 5).Add(new Paragraph("")));
+            table1.AddCell(NoBorderCell("Fecha de nacimiento:"));
+            table1.AddCell(NoBorderCell("", 5).SetUnderline());
 
-            table1.AddCell("Parentesco:");
-            table1.AddCell("MADRE");
-            table1.AddCell("Domicilio del beneficiario:");
-            table1.AddCell(new Cell(1, 3).Add(new Paragraph("AV LAS TORRES 2308 COL LAZARO CARDENAS CP. 89602 ALTAMIRA TAMPS.")));
+            table1.AddCell(NoBorderCell("Parentesco:"));
+            table1.AddCell(NoBorderCell("MADRE").SetUnderline());
+            table1.AddCell(NoBorderCell("Domicilio del beneficiario:"));
+            table1.AddCell(NoBorderCell("AV LAS TORRES 2308 COL LAZARO CARDENAS CP. 89602 ALTAMIRA TAMPS.", 3).SetUnderline());
 
-            table1.AddCell("Cuenta Bancaria:");
-            table1.AddCell("");
-            table1.AddCell("Tarjeta / Clabe:");
-            table1.AddCell("");
-            table1.AddCell("Banco:");
-            table1.AddCell("");
+            table1.AddCell(NoBorderCell("Cuenta Bancaria:"));
+            table1.AddCell(NoBorderCell("").SetUnderline());
+            table1.AddCell(NoBorderCell("Tarjeta / Clabe:"));
+            table1.AddCell(NoBorderCell("").SetUnderline());
+            table1.AddCell(NoBorderCell("Banco:"));
+            table1.AddCell(NoBorderCell("").SetUnderline());
+
 
             document.Add(table1);
             document.Add(new Paragraph().SetHeight(10)); // Spacer
@@ -171,7 +173,17 @@ namespace EngineeringManagement.Core.Services
 
             document.Add(headerTable);
 
-            document.Add(new Paragraph("\n"));
+            document.Add(new Paragraph().SetHeight(10)); // Spacer
+        }
+
+        private static Cell NoBorderCell(string text)
+        {
+            return new Cell().SetBorder(Border.NO_BORDER).Add(new Paragraph(text));
+        }
+
+        private static Cell NoBorderCell(string text, int colspan)
+        {
+            return new Cell(1, colspan).SetBorder(Border.NO_BORDER).Add(new Paragraph(text));
         }
     }
 }
