cf71b9a1b8908ef3fa86a0a768ce44279dca8728
Gerardo Aguilar <gerardo.aguilar@petco.com>
2025-08-07 16:11:03 -0600

    WIP. Implemented first section with different row styles

--- EngineeringManagement.Core/Services/PdfGeneratorService.cs
+++ EngineeringManagement.Core/Services/PdfGeneratorService.cs
@@ -14,12 +14,13 @@ namespace EngineeringManagement.Core.Services
 {
     public class PdfGeneratorService : IPdfGeneratorService
     {
-        private static readonly PdfFont boldFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
-        private static readonly PdfFont regularFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
         private readonly string logoPath;
 
         public void Generate(GeneralEmployee employee, string basePath, string logoPath)
         {
+            var boldFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+            var regularFont = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+
             if (!Directory.Exists(basePath))
             {
                 Directory.CreateDirectory(basePath);
@@ -29,109 +30,102 @@ namespace EngineeringManagement.Core.Services
             using var pdf = new PdfDocument(writer);
             var document = new Document(pdf);
 
-            AddHeader(document, logoPath);
+            AddHeader(document, logoPath, boldFont, regularFont);
 
-            AddSectionTitle(document, "DATOS DE INGRESO DEL EMPLEADO (INFORMACIÓN LLENADA POR EL EMPLEADO)");
-
-            AddFirstSection(document);
+            AddFirstSection(document, boldFont, regularFont);
 
             document.Close();
         }
 
-        private static void AddFirstSection(Document document)
+        private static void AddFirstSection(Document document, PdfFont boldFont, PdfFont regularFont)
         {
             // First block of info (2-column layout)
-            var table1 = new Table(2).UseAllAvailableWidth();
+            var tableTitle = new Table(1).UseAllAvailableWidth();
+            tableTitle.AddCell(new Cell()
+                .Add(new Paragraph("DATOS DE INGRESO DEL EMPLEADO (INFORMACIÓN LLENADA POR EL EMPLEADO)")
+                .SetFont(boldFont).SetFontSize(10))
+                .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
+                .SetTextAlignment(TextAlignment.CENTER)
+                .SetPadding(5));
+            document.Add(tableTitle);
+
+            // Flexible info table layout (6 columns)
+            var table1 = new Table(6).UseAllAvailableWidth();
 
             table1.AddCell("Nombre:");
             table1.AddCell("ERICK EUGENIO DEL VALLE BALDERAS");
-
             table1.AddCell("Estado civil:");
             table1.AddCell("SOLTERO");
-
             table1.AddCell("Correo:");
             table1.AddCell("");
 
             table1.AddCell("Domicilio:");
-            table1.AddCell("AV LAS TORRES 2308 COL LAZARO CARDENAS CP. 89602 ALTAMIRA TAMPS.");
+            table1.AddCell(new Cell(1, 5).Add(new Paragraph("AV LAS TORRES 2308 COL LAZARO CARDENAS CP. 89602 ALTAMIRA TAMPS.")));
 
             table1.AddCell("NSS:");
             table1.AddCell("38179900907");
-
             table1.AddCell("No. Clínica IMSS:");
             table1.AddCell("10");
-
             table1.AddCell("Teléfono:");
             table1.AddCell("833 323 5657");
 
             table1.AddCell("No. Crédito Infonavit:");
             table1.AddCell("NO APLICA");
-
             table1.AddCell("% - VSM - Cuota:");
             table1.AddCell("NO APLICA");
-
             table1.AddCell("Número de cliente (Fonacot):");
             table1.AddCell("NO APLICA");
 
             table1.AddCell("CURP:");
             table1.AddCell("VABE990508HTSLLR05");
-
             table1.AddCell("RFC:");
             table1.AddCell("VABE9905088J4");
-
             table1.AddCell("Fecha de Nacimiento:");
             table1.AddCell("08/05/1999");
 
             table1.AddCell("Lugar de Nacimiento:");
-            table1.AddCell("CD MADERO TAMPS.");
+            table1.AddCell(new Cell(1, 5).Add(new Paragraph("CD MADERO TAMPS.")));
 
             table1.AddCell("Grado acad.:");
             table1.AddCell("");
-
             table1.AddCell("Documento:");
             table1.AddCell("");
-
             table1.AddCell("Profesión:");
             table1.AddCell("");
 
             table1.AddCell("Nombre del padre:");
-            table1.AddCell("JAIME DEL VALLE OLMEDO");
+            table1.AddCell(new Cell(1, 5).Add(new Paragraph("JAIME DEL VALLE OLMEDO")));
 
             table1.AddCell("Nombre de la madre:");
-            table1.AddCell("ZOILA BALDERAS RAMIREZ");
+            table1.AddCell(new Cell(1, 5).Add(new Paragraph("ZOILA BALDERAS RAMIREZ")));
 
             table1.AddCell("Beneficiario:");
             table1.AddCell("ZOILA BALDERAS RAMIREZ");
-
             table1.AddCell("%:");
             table1.AddCell("100");
-
             table1.AddCell("Teléfono beneficiario:");
             table1.AddCell("833 104 2230");
 
             table1.AddCell("Fecha de nacimiento:");
-            table1.AddCell("");
+            table1.AddCell(new Cell(1, 5).Add(new Paragraph("")));
 
             table1.AddCell("Parentesco:");
             table1.AddCell("MADRE");
-
             table1.AddCell("Domicilio del beneficiario:");
-            table1.AddCell("AV LAS TORRES 2308 COL LAZARO CARDENAS CP. 89602 ALTAMIRA TAMPS.");
+            table1.AddCell(new Cell(1, 3).Add(new Paragraph("AV LAS TORRES 2308 COL LAZARO CARDENAS CP. 89602 ALTAMIRA TAMPS.")));
 
             table1.AddCell("Cuenta Bancaria:");
             table1.AddCell("");
-
             table1.AddCell("Tarjeta / Clabe:");
             table1.AddCell("");
-
             table1.AddCell("Banco:");
             table1.AddCell("");
 
             document.Add(table1);
-            document.Add(new Paragraph().SetHeight(10));
+            document.Add(new Paragraph().SetHeight(10)); // Spacer
         }
 
-        private static void AddHeader(Document document, string logoPath)
+        private static void AddHeader(Document document, string logoPath, PdfFont boldFont, PdfFont regularFont)
         {
             var headerTable = new Table([1, 2, 3]).UseAllAvailableWidth();
 
@@ -178,22 +172,6 @@ namespace EngineeringManagement.Core.Services
             document.Add(headerTable);
 
             document.Add(new Paragraph("\n"));
-        }
-
-        private static void AddSectionTitle(Document document, string titleText)
-        {
-            var table = new Table(1).UseAllAvailableWidth();
-
-            var titleCell = new Cell()
-                .Add(new Paragraph(titleText).SetFont(boldFont).SetFontSize(12))
-                .SetBackgroundColor(ColorConstants.LIGHT_GRAY)
-                .SetTextAlignment(TextAlignment.CENTER)
-                .SetVerticalAlignment(VerticalAlignment.MIDDLE)
-                .SetBorder(new SolidBorder(1))
-                .SetPadding(5);
-
-            table.AddCell(titleCell);
-            document.Add(table);
         }
     }
 }
