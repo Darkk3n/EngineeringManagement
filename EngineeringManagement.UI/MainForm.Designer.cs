be68fa7cddc4ae60f3617e90f9d78c72715c8997
Gerardo Aguilar <gerardo.aguilar@petco.com>
2025-11-28 23:17:34 -0600

    Disabled menu option for the time being

--- EngineeringManagement.UI/MainForm.Designer.cs
+++ EngineeringManagement.UI/MainForm.Designer.cs
@@ -37,6 +37,7 @@
             editarEmpleadoToolStripMenuItem = new ToolStripMenuItem();
             expedientesMedicosToolStripMenuItem = new ToolStripMenuItem();
             generarContratoToolStripMenuItem = new ToolStripMenuItem();
+            generarAlcanceToolStripMenuItem = new ToolStripMenuItem();
             plantasToolStripMenuItem = new ToolStripMenuItem();
             agregarPlantaToolStripMenuItem = new ToolStripMenuItem();
             editarPlantaToolStripMenuItem = new ToolStripMenuItem();
@@ -53,7 +54,6 @@
             BtnExport = new Button();
             btnSearch = new Button();
             btnClear = new Button();
-            generarAlcanceToolStripMenuItem = new ToolStripMenuItem();
             menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvEmployeeList).BeginInit();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
@@ -68,8 +68,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, empleadosToolStripMenuItem, plantasToolStripMenuItem, dC3ToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Padding = new Padding(7, 3, 0, 3);
-            menuStrip1.Size = new Size(1093, 30);
+            menuStrip1.Size = new Size(956, 24);
             menuStrip1.TabIndex = 1;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -77,13 +76,13 @@
             // 
             toolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { salirToolStripMenuItem });
             toolStripMenuItem1.Name = "toolStripMenuItem1";
-            toolStripMenuItem1.Size = new Size(73, 24);
+            toolStripMenuItem1.Size = new Size(60, 20);
             toolStripMenuItem1.Text = "Archivo";
             // 
             // salirToolStripMenuItem
             // 
             salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            salirToolStripMenuItem.Size = new Size(224, 26);
+            salirToolStripMenuItem.Size = new Size(96, 22);
             salirToolStripMenuItem.Text = "Salir";
             salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
             // 
@@ -91,54 +90,62 @@
             // 
             empleadosToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { agregarEmpleadoToolStripMenuItem, editarEmpleadoToolStripMenuItem, expedientesMedicosToolStripMenuItem, generarContratoToolStripMenuItem, generarAlcanceToolStripMenuItem });
             empleadosToolStripMenuItem.Name = "empleadosToolStripMenuItem";
-            empleadosToolStripMenuItem.Size = new Size(97, 24);
+            empleadosToolStripMenuItem.Size = new Size(77, 20);
             empleadosToolStripMenuItem.Text = "Empleados";
             // 
             // agregarEmpleadoToolStripMenuItem
             // 
             agregarEmpleadoToolStripMenuItem.Name = "agregarEmpleadoToolStripMenuItem";
-            agregarEmpleadoToolStripMenuItem.Size = new Size(232, 26);
+            agregarEmpleadoToolStripMenuItem.Size = new Size(184, 22);
             agregarEmpleadoToolStripMenuItem.Text = "Agregar Empleado";
             agregarEmpleadoToolStripMenuItem.Click += agregarEmpleadoToolStripMenuItem_Click;
             // 
             // editarEmpleadoToolStripMenuItem
             // 
             editarEmpleadoToolStripMenuItem.Name = "editarEmpleadoToolStripMenuItem";
-            editarEmpleadoToolStripMenuItem.Size = new Size(232, 26);
+            editarEmpleadoToolStripMenuItem.Size = new Size(184, 22);
             editarEmpleadoToolStripMenuItem.Text = "Editar Empleado";
             editarEmpleadoToolStripMenuItem.Click += editarEmpleadoToolStripMenuItem_Click;
             // 
             // expedientesMedicosToolStripMenuItem
             // 
             expedientesMedicosToolStripMenuItem.Name = "expedientesMedicosToolStripMenuItem";
-            expedientesMedicosToolStripMenuItem.Size = new Size(232, 26);
+            expedientesMedicosToolStripMenuItem.Size = new Size(184, 22);
             expedientesMedicosToolStripMenuItem.Text = "Expedientes Medicos";
             expedientesMedicosToolStripMenuItem.Click += expedientesMedicosToolStripMenuItem_Click;
             // 
             // generarContratoToolStripMenuItem
             // 
+            generarContratoToolStripMenuItem.Enabled = false;
             generarContratoToolStripMenuItem.Name = "generarContratoToolStripMenuItem";
-            generarContratoToolStripMenuItem.Size = new Size(232, 26);
+            generarContratoToolStripMenuItem.Size = new Size(184, 22);
             generarContratoToolStripMenuItem.Text = "Generar Contrato";
+            // 
+            // generarAlcanceToolStripMenuItem
+            // 
+            generarAlcanceToolStripMenuItem.Name = "generarAlcanceToolStripMenuItem";
+            generarAlcanceToolStripMenuItem.Size = new Size(184, 22);
+            generarAlcanceToolStripMenuItem.Text = "Generar Alcance";
+            generarAlcanceToolStripMenuItem.Click += generarAlcanceToolStripMenuItem_Click;
             // 
             // plantasToolStripMenuItem
             // 
             plantasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { agregarPlantaToolStripMenuItem, editarPlantaToolStripMenuItem });
             plantasToolStripMenuItem.Name = "plantasToolStripMenuItem";
-            plantasToolStripMenuItem.Size = new Size(70, 24);
+            plantasToolStripMenuItem.Size = new Size(57, 20);
             plantasToolStripMenuItem.Text = "Plantas";
             // 
             // agregarPlantaToolStripMenuItem
             // 
             agregarPlantaToolStripMenuItem.Name = "agregarPlantaToolStripMenuItem";
-            agregarPlantaToolStripMenuItem.Size = new Size(191, 26);
+            agregarPlantaToolStripMenuItem.Size = new Size(152, 22);
             agregarPlantaToolStripMenuItem.Text = "Agregar Planta";
             agregarPlantaToolStripMenuItem.Click += agregarPlantaToolStripMenuItem_Click;
             // 
             // editarPlantaToolStripMenuItem
             // 
             editarPlantaToolStripMenuItem.Name = "editarPlantaToolStripMenuItem";
-            editarPlantaToolStripMenuItem.Size = new Size(191, 26);
+            editarPlantaToolStripMenuItem.Size = new Size(152, 22);
             editarPlantaToolStripMenuItem.Text = "Editar Planta";
             editarPlantaToolStripMenuItem.Click += editarPlantaToolStripMenuItem_Click;
             // 
@@ -146,37 +153,36 @@
             // 
             dC3ToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { agregarDC3ToolStripMenuItem, editarDC3ToolStripMenuItem, agregarDC3AEmpleadoToolStripMenuItem });
             dC3ToolStripMenuItem.Name = "dC3ToolStripMenuItem";
-            dC3ToolStripMenuItem.Size = new Size(57, 24);
+            dC3ToolStripMenuItem.Size = new Size(46, 20);
             dC3ToolStripMenuItem.Text = "DC-3";
             // 
             // agregarDC3ToolStripMenuItem
             // 
             agregarDC3ToolStripMenuItem.Name = "agregarDC3ToolStripMenuItem";
-            agregarDC3ToolStripMenuItem.Size = new Size(268, 26);
+            agregarDC3ToolStripMenuItem.Size = new Size(211, 22);
             agregarDC3ToolStripMenuItem.Text = "Agregar DC-3";
             agregarDC3ToolStripMenuItem.Click += agregarDC3ToolStripMenuItem_Click;
             // 
             // editarDC3ToolStripMenuItem
             // 
             editarDC3ToolStripMenuItem.Name = "editarDC3ToolStripMenuItem";
-            editarDC3ToolStripMenuItem.Size = new Size(268, 26);
+            editarDC3ToolStripMenuItem.Size = new Size(211, 22);
             editarDC3ToolStripMenuItem.Text = "Editar DC-3";
             editarDC3ToolStripMenuItem.Click += editarDC3ToolStripMenuItem_Click;
             // 
             // agregarDC3AEmpleadoToolStripMenuItem
             // 
             agregarDC3AEmpleadoToolStripMenuItem.Name = "agregarDC3AEmpleadoToolStripMenuItem";
-            agregarDC3AEmpleadoToolStripMenuItem.Size = new Size(268, 26);
+            agregarDC3AEmpleadoToolStripMenuItem.Size = new Size(211, 22);
             agregarDC3AEmpleadoToolStripMenuItem.Text = "Agregar DC-3 a Empleado";
             agregarDC3AEmpleadoToolStripMenuItem.Click += agregarDC3AEmpleadoToolStripMenuItem_Click;
             // 
             // cmbEmployees
             // 
             cmbEmployees.FormattingEnabled = true;
-            cmbEmployees.Location = new Point(14, 40);
-            cmbEmployees.Margin = new Padding(3, 4, 3, 4);
+            cmbEmployees.Location = new Point(12, 30);
             cmbEmployees.Name = "cmbEmployees";
-            cmbEmployees.Size = new Size(367, 28);
+            cmbEmployees.Size = new Size(322, 23);
             cmbEmployees.TabIndex = 2;
             cmbEmployees.SelectedIndexChanged += cmbEmployees_SelectedIndexChanged;
             cmbEmployees.TextChanged += cmbEmployees_TextChanged;
@@ -187,20 +193,18 @@
             dgvEmployeeList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvEmployeeList.Dock = DockStyle.Fill;
             dgvEmployeeList.Location = new Point(0, 0);
-            dgvEmployeeList.Margin = new Padding(3, 4, 3, 4);
             dgvEmployeeList.Name = "dgvEmployeeList";
             dgvEmployeeList.RowHeadersWidth = 51;
-            dgvEmployeeList.Size = new Size(1093, 736);
+            dgvEmployeeList.Size = new Size(956, 551);
             dgvEmployeeList.TabIndex = 3;
             // 
             // btnAdd
             // 
             btnAdd.Image = Properties.Resources.business_application_addmale_useradd_insert_add_user_client_2312__1_;
             btnAdd.ImageAlign = ContentAlignment.MiddleLeft;
-            btnAdd.Location = new Point(457, 25);
-            btnAdd.Margin = new Padding(3, 4, 3, 4);
+            btnAdd.Location = new Point(400, 19);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(128, 57);
+            btnAdd.Size = new Size(112, 43);
             btnAdd.TabIndex = 4;
             btnAdd.Text = "Agregar/Editar";
             btnAdd.TextAlign = ContentAlignment.MiddleRight;
@@ -211,10 +215,9 @@
             // 
             btnDelete.Image = Properties.Resources.vcsconflicting_93497__1_;
             btnDelete.ImageAlign = ContentAlignment.MiddleLeft;
-            btnDelete.Location = new Point(592, 27);
-            btnDelete.Margin = new Padding(3, 4, 3, 4);
+            btnDelete.Location = new Point(518, 20);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(88, 57);
+            btnDelete.Size = new Size(77, 43);
             btnDelete.TabIndex = 5;
             btnDelete.Text = "Eliminar";
             btnDelete.TextAlign = ContentAlignment.MiddleRight;
@@ -224,17 +227,16 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(14, 9);
+            label1.Location = new Point(12, 7);
             label1.Name = "label1";
-            label1.Size = new Size(86, 20);
+            label1.Size = new Size(68, 15);
             label1.TabIndex = 6;
             label1.Text = "Empleados:";
             // 
             // splitContainer1
             // 
             splitContainer1.Dock = DockStyle.Fill;
-            splitContainer1.Location = new Point(0, 30);
-            splitContainer1.Margin = new Padding(3, 4, 3, 4);
+            splitContainer1.Location = new Point(0, 24);
             splitContainer1.Name = "splitContainer1";
             splitContainer1.Orientation = Orientation.Horizontal;
             // 
@@ -251,17 +253,15 @@
             // splitContainer1.Panel2
             // 
             splitContainer1.Panel2.Controls.Add(dgvEmployeeList);
-            splitContainer1.Size = new Size(1093, 837);
-            splitContainer1.SplitterDistance = 96;
-            splitContainer1.SplitterWidth = 5;
+            splitContainer1.Size = new Size(956, 626);
+            splitContainer1.SplitterDistance = 71;
             splitContainer1.TabIndex = 7;
             // 
             // BtnExport
             // 
-            BtnExport.Location = new Point(687, 27);
-            BtnExport.Margin = new Padding(3, 4, 3, 4);
+            BtnExport.Location = new Point(601, 20);
             BtnExport.Name = "BtnExport";
-            BtnExport.Size = new Size(88, 57);
+            BtnExport.Size = new Size(77, 43);
             BtnExport.TabIndex = 9;
             BtnExport.Text = "Exportar a Excel";
             BtnExport.UseVisualStyleBackColor = true;
@@ -270,44 +270,34 @@
             // btnSearch
             // 
             btnSearch.Image = Properties.Resources.Lupa_Icon__2_;
-            btnSearch.Location = new Point(389, 39);
-            btnSearch.Margin = new Padding(3, 4, 3, 4);
+            btnSearch.Location = new Point(340, 29);
             btnSearch.Name = "btnSearch";
-            btnSearch.Size = new Size(38, 32);
+            btnSearch.Size = new Size(33, 24);
             btnSearch.TabIndex = 8;
             btnSearch.UseVisualStyleBackColor = true;
             btnSearch.Click += btnSearch_Click;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(782, 25);
-            btnClear.Margin = new Padding(3, 4, 3, 4);
+            btnClear.Location = new Point(684, 19);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(88, 57);
+            btnClear.Size = new Size(77, 43);
             btnClear.TabIndex = 7;
             btnClear.Text = "Anular Seleccion";
             btnClear.UseVisualStyleBackColor = true;
             btnClear.Click += btnClear_Click;
             // 
-            // generarAlcanceToolStripMenuItem
-            // 
-            generarAlcanceToolStripMenuItem.Name = "generarAlcanceToolStripMenuItem";
-            generarAlcanceToolStripMenuItem.Size = new Size(232, 26);
-            generarAlcanceToolStripMenuItem.Text = "Generar Alcance";
-            generarAlcanceToolStripMenuItem.Click += generarAlcanceToolStripMenuItem_Click;
-            // 
             // MainForm
             // 
             AcceptButton = btnSearch;
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1093, 867);
+            ClientSize = new Size(956, 650);
             Controls.Add(splitContainer1);
             Controls.Add(menuStrip1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
             MainMenuStrip = menuStrip1;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "CONTROL TOTAL INGENIERIA";
